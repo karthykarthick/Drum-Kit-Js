ddea9a81bf7c69ae8289f34bb2b5e1e0a7424cce
karthick <karthylock@gmail.com>
2021-02-17 18:32:06 +0530

    added cases to play music

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
-for(var i =0; i < document.querySelectorAll(".drum").length;i++){
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document
+    .querySelectorAll(".drum")
+    [i].addEventListener("click", function() {
+      var buttonInnerHtml = this.innerHTML;
+       
+      switch (buttonInnerHtml) {
+        case "w":
+          var crash = new Audio("./sounds/crash.mp3");
+          crash.play();
+        break;
 
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-    
-    function handleClick (){
-        // alert("Hello you re clicked");
-        var audio = new Audio("./sounds/tom-1.mp3");
-        audio.play();
-    }
+        case "a":
+          var kick = new Audio("./sounds/kick-bass.mp3");
+          kick.play();
+        break;
+
+        case "s":
+          var snare = new Audio("./sounds/snare.mp3");
+          snare.play();
+        break;
+
+        case "d":
+          var tom1 = new Audio("./sounds/tom-1.mp3");
+          tom1.play();
+        break;
+
+        case "j":
+          var tom2 = new Audio("./sounds/tom-2.mp3");
+          tom2.play();
+        break;
+
+        case "k":
+          var tom3 = new Audio("./sounds/tom-3.mp3");
+          tom3.play();
+        break;
+
+        case "l":
+          var tom4 = new Audio("./sounds/tom-4.mp3");
+          tom4.play();
+        break;
+
+        default:
+          console.log(buttonInnerHtml);
+      }
+    });
 }
+
+// // alert("Hello you re clicked");
+// var audio = new Audio("./sounds/tom-1.mp3");
+// audio.play();
